1b5edf555a9d52f189dc4da94c9f2cff51933d9a
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-21 09:24:44 +0000

    vreated components folder and a navbar

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./NavBar.module.css";
+
+export default function NavBar() {
+  return (
+    <div className={css.navBar}>
+      <div className={css.home}>Home</div>
+      <div className={css.drivers}>Drivers</div>
+    </div>
+  );
+}
