196cac2b85447f67fc5c2e31a6b4be727257644e
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-28 18:56:53 +0000

    displaying driver images

--- src/Components/DriverCard/index.js
+++ src/Components/DriverCard/index.js
@@ -3,11 +3,11 @@ import React from "react";
 function DriverCard({ driver }) {
   return (
     <>
-      <h1 key={driver.driverNumber}>{driver.driverName}</h1>
       <img
         src={`${process.env.PUBLIC_URL}/assets/Images/${driver.driverNumber}.png`}
         alt={driver.driverName}
       />
+      <h1 key={driver.driverNumber}>{driver.driverName}</h1>
     </>
   );
 }
