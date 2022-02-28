a99bfdc7a5f65ab2d365d67729ce961994be056b
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-28 18:21:18 +0000

    created some routes with dummy data to show

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,18 @@
 import NavBar from "../NavBar";
+import { Routes, Route } from "react-router-dom";
+import Drivers from "../Drivers";
+import Home from "../Home";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="drivers" element={<Drivers />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import css from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className={css.navBar}>
-      <div className={css.home}>Home</div>
-      <div className={css.drivers}>Drivers</div>
+      <div className={css.home}>
+        <Link to="/">Home</Link>
+      </div>
+      <div className={css.drivers}>
+        <Link to="/drivers">Drivers</Link>
+      </div>
     </div>
   );
 }
