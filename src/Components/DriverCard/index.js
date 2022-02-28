9aa26d5bc6186ef19a0ab88072eda89547f59538
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-28 18:40:01 +0000

    displaying driver images

--- src/Components/DriverCard/index.js
+++ src/Components/DriverCard/index.js
@@ -4,6 +4,10 @@ function DriverCard({ driver }) {
   return (
     <>
       <h1 key={driver.driverNumber}>{driver.driverName}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/Images/${driver.driverNumber}.png`}
+        alt={driver.driverName}
+      />
     </>
   );
 }
